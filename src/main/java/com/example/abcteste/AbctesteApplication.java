79d63b66a946ba0a7b94473695f8002bc52c191d
Luiz Carlos Muniz <lcmuniz@gmail.com>
2021-03-05 18:04:31 -0300

    inicial

---
+++ src/main/java/com/example/abcteste/AbctesteApplication.java
@@ -0,0 +1,13 @@
+package com.example.abcteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbctesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbctesteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/abcteste/AbctesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abcteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbctesteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
